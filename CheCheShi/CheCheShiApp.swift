81b33a1cb59f81bce98b77d23da28a8131e3caef
liqiang38 <liqiang38@58.com>
2024-12-13 16:22:19 +0800

    init project

---
+++ CheCheShi/CheCheShiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheCheShiApp.swift
+//  CheCheShi
+//
+//  Created by Ryan Lee on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct CheCheShiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
